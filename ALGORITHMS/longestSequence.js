6c3794dbf75a776a8c31f962d7ac203d3885f9b0
ammarr <ammarrusesthis80@gmail.com>
2025-12-11 16:01:23 +0500

    Initial commit

---
+++ ALGORITHMS/longestSequence.js
@@ -0,0 +1,24 @@
+/*
+Title: Longest Consecutive Sequence
+
+Description:
+Write a JavaScript program that takes an unsorted array of integers and returns the length of the longest sequence of consecutive numbers.
+The sequence does not need to be sorted in the input array.
+
+Input:
+An unsorted array of integers.
+Example: [100, 4, 200, 1, 3, 2]
+
+Output:
+A single number representing the length of the longest consecutive sequence.
+Example output for the input above: 4
+
+Requirements:
+Do not sort the original array (for the O(n) challenge).
+Use a Set or similar method to efficiently check for existence of numbers.
+Loop through the array to find the start of each sequence and count consecutive numbers.
+
+Input: [100, 4, 200, 1, 3, 2]
+Longest consecutive sequence: [1, 2, 3, 4]
+Output: 4
+*/
