c7c738e1b5104a05b0239f87379734b079be70d4
ammarr <ammarrusesthis80@gmail.com>
2025-12-12 07:18:40 +0500

    First attempt. Works with consecutive values, but causes an error when bigger values are added.

--- ALGORITHMS/longestSequence.js
+++ ALGORITHMS/longestSequence.js
@@ -22,3 +22,30 @@ Input: [100, 4, 200, 1, 3, 2]
 Longest consecutive sequence: [1, 2, 3, 4]
 Output: 4
 */
+
+
+let array = [1, 3, 2, 4];
+
+let nearbyValues = [];
+let prevValue = null;
+let smaller = [];
+let bigger = [];
+let sequence = 0;
+
+const set = new Set(array);
+
+array.map((digit, index) => {
+    let number = digit;
+
+    let next = set.has(number + 1);
+    let prev = set.has(number - 1);
+
+    
+    if (next && prev) {
+        set.add(digit - 1);
+        set.add(digit);
+        set.add(digit + 1);
+    };
+    
+});
+console.log(set.size);
